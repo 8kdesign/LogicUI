12bb81354ccbfe099643c4ac3e46119a8d500676
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-02-18 21:03:32 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import BannerImage from "../assets/logo_banner.png";
 export default function Home() {
 	return (
 		<>
@@ -10,8 +9,12 @@ export default function Home() {
 					content="initial-scale=1.0, width=device-width"
 				/>
 				<meta property="og:title" content="8K | LogicUI" />
+				<meta property="description" content="Official blog by 8K" />
 				<meta property="og:description" content="Official blog by 8K" />
-				<meta property="og:image" content={BannerImage} />
+				<meta
+					property="og:image"
+					content="https://firebasestorage.googleapis.com/v0/b/logicui-9667a.appspot.com/o/others%2Flogo_banner.png?alt=media&token=8522ecea-b68a-4ced-a4df-538c1d8a92db"
+				/>
 			</Head>
 		</>
 	);
