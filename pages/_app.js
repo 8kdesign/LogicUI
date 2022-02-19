8210ede7af48f6049705577ba9d203a6a46b5f31
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-02-19 14:48:24 +0800

    Revert "Add music player screen"
    This reverts commit 92f7563f6a19229b6d82c7585a3bdeded5c9fcf8.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/globals.css";
-import "../App.css";
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
