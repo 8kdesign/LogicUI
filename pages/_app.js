b747c0aee7748ac88cd548d4ed859d175b5115a9
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-02-19 21:00:03 +0800

    Add nav bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 import "../App.css";
 
+import { Navbar, Container } from "react-bootstrap";
+import Link from "next/link";
+import Image from "next/image";
+import ImageLogo from "../assets/inverted_logo.png";
+
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<div className="Container--navbar">
+				<Navbar bg="dark" variant="dark">
+					<Container>
+						<div style={{ cursor: "pointer" }}>
+							<Link href="/">
+								<Image
+									src={ImageLogo}
+									className="Image--logo"
+									height={40}
+									width={40}
+								/>
+							</Link>
+						</div>
+					</Container>
+				</Navbar>
+			</div>
+			<div className="Container--content">
+				<Component {...pageProps} />
+			</div>
+		</>
+	);
 }
 
 export default MyApp;
