941d38b9624632af0d0012d6908c944053876188
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-02-20 10:05:48 +0800

    Test favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,51 +3,20 @@ import "../styles/globals.css";
 import "../App.css";
 
 import AppBar from "../components/AppBar";
-import { useState } from "react";
-import { Nav, Offcanvas } from "react-bootstrap";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-	const [showOffCanvas, setShowOffCanvas] = useState(false);
-
 	return (
 		<>
-			<AppBar setShowOffCanvas={setShowOffCanvas} />
+			<Head>
+				<link rel="shortcut icon" href="/favicon.ico" />
+			</Head>
+			<AppBar />
 			<div className="Container--content">
 				<Component {...pageProps} />
 			</div>
-			<MobileOffCanvas
-				showOffCanvas={showOffCanvas}
-				setShowOffCanvas={setShowOffCanvas}
-			/>
 		</>
 	);
 }
 
 export default MyApp;
-
-function MobileOffCanvas({ showOffCanvas, setShowOffCanvas }) {
-	return (
-		<Offcanvas
-			show={showOffCanvas}
-			onHide={() => setShowOffCanvas(false)}
-			style={{ backgroundColor: "#212529", maxWidth: "80%" }}
-		>
-			<Offcanvas.Body>
-				<Offcanvas.Header closeButton closeVariant="white">
-					<Offcanvas.Title />
-				</Offcanvas.Header>
-				<Nav className="flex-column">
-					<Nav.Link href="/">
-						<p className="Text--menu">Home</p>
-					</Nav.Link>
-					<Nav.Link href="/logicui">
-						<p className="Text--menu">LogicUI Apps</p>
-					</Nav.Link>
-					<Nav.Link href="/reviews">
-						<p className="Text--menu">Reviews</p>
-					</Nav.Link>
-				</Nav>
-			</Offcanvas.Body>
-		</Offcanvas>
-	);
-}
