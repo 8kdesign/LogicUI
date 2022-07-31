e0cc3241eb0018b5b9e321c02b9b2a07173266f1
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2022-07-31 21:54:38 +0800

    Revert "Purge attempt"
    This reverts commit 150d499061634f16c65218de724cb0d9833e90e0.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
 
-module.exports = withCss(
-	withPurgeCss({
-		purgeCss: {
-			safelist: {
-				standard: ["html", "body", "btn"],
-				deep: [/^col/, /^row/, /^container/, /^tooltip/, /^modal/],
-			},
-		},
-	})
-);
+module.exports = withCss(withPurgeCss());
